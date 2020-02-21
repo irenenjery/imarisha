b73f78d8826da5005d20f44beaa88dc4e794d844
dawiditer <dawiditer@gmail.com>
2020-02-21 19:46:51 +0300

    Removes unnecessary files

--- auth.php
+++ auth.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <?php require 'includes/db/connectdb_imarisha.php'; ?>
 <?php require 'includes/db/db_functions.php' ?>
 <?php require 'includes/helper_functions.php'; ?>
@@ -18,15 +17,12 @@ if ( substr_compare(substr($username, 0, 1), '@', 0) == 0 ) {
 	$user = getAuthCoach($conn, "coach_username='".$username."' OR coach_email='".$username."'");
   if ( password_verify($pass, $user['coach_pass']) ) {//password matches
 		$mismatch = false;
-		$_SESSION["user_type"] = "coach";
 		$user_id = $user['coach_id'];
   }
 } else {
-	$user_type = "coach";
 	$user = getAuthClient($conn, "client_username='".$username."' OR client_email='".$username."'");
   if ( password_verify($pass, $user['client_pass']) ) {//password matches
 		$mismatch = false;
-		$_SESSION["user_type"] = "client";
 		$user_id = $user['client_id'];
   }
 }
@@ -34,10 +30,10 @@ if ( substr_compare(substr($username, 0, 1), '@', 0) == 0 ) {
 //username/email doesn't exist or password doesn't match
 if ( $mismatch  ) {
 	redirect('login.php', 'auth_fail=match_error');
-} 
+} else {
+	echo 'client/coach homepage';
+}
 
-$_SESSION["user"] = $user;
-redirect('temp.php', 'user_id='.$user_id);//redirect to client/coach
 
 mysqli_close($conn);
 ?>
